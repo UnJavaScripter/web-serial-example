2c42cde2614abcf782bfaae1f8b5651b27616b9d
UnJavaScripter <diegocoyt@gmail.com>
2020-04-09 17:15:59 +0200

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,38 @@
+"use strict";
+class SerialLEDController {
+    constructor() {
+        this.encoder = new TextEncoder();
+        this.decoder = new TextDecoder();
+    }
+    async init() {
+        if ('serial' in navigator) {
+            try {
+                const port = await navigator.serial.requestPort();
+                await port.open({ baudrate: 9600 });
+                this.reader = port.readable.getReader();
+                this.writer = port.writable.getWriter();
+            }
+            catch (err) {
+                console.error('There was an error opening the serial port:', err);
+            }
+        }
+        else {
+            console.error('Web serial doesn\'t seem to be enabled in your browser. Try enabling it (if Chrome, visit chrome://flags/#enable-experimental-web-platform-features).');
+        }
+    }
+    async write(data) {
+        const dataArrayBuffer = this.encoder.encode(data);
+        return await this.writer.write(dataArrayBuffer);
+    }
+    async read() {
+        try {
+            const readerData = await this.reader.read();
+            return this.decoder.decode(readerData.value);
+        }
+        catch (err) {
+            const errorMessage = `error reading data: ${err}`;
+            console.error(errorMessage);
+            return errorMessage;
+        }
+    }
+}
